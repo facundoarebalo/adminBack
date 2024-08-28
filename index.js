daa2318a8b9552abeb17a2318b6986af34dba75f
Facundo Arebalo <facuar1589@gmail.com>
2024-08-28 12:05:16 -0300

    configuraciones

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-import express from 'express';
+import express from "express";
+import "dotenv/config";
 
-const app = express();  
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const initApp = () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`Servidor iniciando en el puerto: ${PORT}`);
+    });
+  } catch (error) {
+    console.error(`Error starting server: ${error}`);
+  }
+};
+
+initApp();
