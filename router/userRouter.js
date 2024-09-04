7b1f9e10e69259aa2016ef3d3f0991f0a0565f6c
Facundo Arebalo <facuar1589@gmail.com>
2024-09-04 17:37:56 -0300

    fix usuarios

--- router/userRouter.js
+++ router/userRouter.js
@@ -15,29 +15,10 @@ router.get("/users", async (req, res) => {
 });
 
 // traer un usuario
-router.get("/users/:id", async (req, res) => {
-  try {
-    const user = await UsersModel.findById(req.params.id);
-    if (!user) return res.status(404).json({ message: "User not found" });
-    res.json(user);
-    console.log(user);
-  } catch (error) {
-    console.log(error);
-  }
-});
+
 
 // crear un usuario
 
-router.post("/users", async (req, res) => {
-  const newUser = new UsersModel(req.body);
-  try {
-    const savedUser = await newUser.save();
-    res.json(savedUser);
-    console.log(savedUser);
-  } catch (error) {
-    console.log(error);
-  }
-});
 
 // actualizar un usuario
 
