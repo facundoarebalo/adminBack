212d07d85f544f2f74f53c0981c38b702aaf9de4
Facundo Arebalo <facuar1589@gmail.com>
2024-09-05 23:10:20 -0300

    funcion update y delete

--- router/userRouter.js
+++ router/userRouter.js
@@ -16,12 +16,59 @@ router.get("/users", async (req, res) => {
 
 // traer un usuario
 
+router.get("/users/:id", async (req, res) => {
+  try {
+    const user = await UsersModel.findById(req.params.id);
+    if (!user) return res.status(404).json({ message: "User not found" });
+    res.json(user);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 
 // crear un usuario
+
+router.post("/users", async (req, res) => {
+  const newUser = new UsersModel(req.body);
+
+  try {
+    const savedUser = await newUser.save();
+    res.json(savedUser);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 
 // actualizar un usuario
 
+router.put("/users/:id", async (req, res) => {
+  try {
+    const user = await UsersModel.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    res.json(user);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 // eliminar un usuario
+
+router.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await UsersModel.findByIdAndDelete(req.params.id);
+
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    res.json({ message: "User deleted successfully" });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 export default router;
